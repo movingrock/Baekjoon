15c9b30c11dc5afaa4eca907149539da299080b7
movingrock <75603892+movingrock@users.noreply.github.com>
2023-12-29 20:08:04 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    time_t timer;
+    struct tm* t;
+    timer = time(NULL);
+    t = localtime(&timer);
+    
+    cout << t->tm_year+1900 << "-" << t->tm_mon+1 << "-" << t->tm_mday;
+
+    return 0;
+}
