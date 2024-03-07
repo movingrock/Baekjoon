cc5714f3967907cc03e759962c4c97b950ada4c9
movingrock <75603892+movingrock@users.noreply.github.com>
2024-03-07 16:34:34 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	priority_queue<int, vector<int>, greater<int>> pq;
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		if (x == 0) {
+			if (pq.empty()) cout << 0 << "\n";
+			else {
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(x);
+		}
+	}
+
+
+	return 0;
+}
