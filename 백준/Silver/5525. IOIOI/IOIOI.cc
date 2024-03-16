7c5f8cc054d72386f98dad9cd3ca853e1841ca81
movingrock <75603892+movingrock@users.noreply.github.com>
2024-03-16 13:52:42 +0900

    [Silver I] Title: IOIOI, Time: 8 ms, Memory: 3680 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -8,24 +8,24 @@ int main() {
 	int n, m; string str;
 	cin >> n >> m >> str;
 
-	string p = "I";
-	for (int i = 0; i < 2* n; i++) {
-		if (i % 2 == 0) p += "O";
-		else p += "I";
-	}
-	
 	int cnt = 0;
 
-	for (int i = 0; i < str.length() - n * 2; i++) {
-		bool isRight = true;
-		int ccc = 0;
-		for (int j = i; j < i + (n * 2 + 1); j++) {
-			if (str[j] != p[ccc]) isRight = false;
-			ccc++;
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] == 'O') continue;
+
+		int k = 0;
+		while (str[i + 1] == 'O' && str[i + 2] == 'I') {
+			k++;
+
+			if (k == n) {
+				cnt++;
+				k--;
+			}
+			i += 2;
 		}
-		if (isRight) cnt++;
 	}
 
 	cout << cnt;
+
 	return 0;
 }
