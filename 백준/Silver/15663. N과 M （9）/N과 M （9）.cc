b34948f2315539a226e73f6ae0b9f1cac4c4e919
movingrock <75603892+movingrock@users.noreply.github.com>
2024-03-19 22:05:48 +0900

    [Silver II] Title: N과 M (9), Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr[9];
+int input[9];
+bool visited[9];
+int n, m;
+
+void backtracking(int cnt, int x) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	int bNum = 0;
+
+	for (int i = 0; i < n; i++) {
+		if (!visited[i] && input[i] != bNum) {
+			arr[cnt] = input[i];
+			bNum = input[i];
+			visited[i] = true;
+			backtracking(cnt + 1, x + 1);
+			visited[i] = false;
+		}
+	}
+}
+
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		cin >> input[i];
+	}
+	sort(input, input + n);
+
+	backtracking(0, 0);
+
+	return 0;
+}
