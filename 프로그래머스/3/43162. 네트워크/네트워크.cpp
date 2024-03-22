7b260380883ee3b9bbc245f3f6ce71868523929f
movingrock <75603892+movingrock@users.noreply.github.com>
2024-03-22 21:04:37 +0900

    [level 3] Title: 네트워크, Time: 0.03 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool visited[201] = {0,};
+
+void dfs(int x, vector<vector<int>>& computers){
+    visited[x] = true;
+    for(int i = 0; i<computers.size(); i++){
+        if(computers[x][i] == 1 && !visited[i]){
+            dfs(i, computers);
+        }
+    }
+}
+
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+    
+    for(int i= 0; i<n; i++){
+        if(!visited[i]) {
+            dfs(i, computers); answer++;}
+    }
+    
+    return answer;
+}
