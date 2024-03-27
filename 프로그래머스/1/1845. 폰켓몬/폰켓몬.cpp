b8be407f55e0fd573b8d4bb8b5b9bbea0db36cd1
movingrock <75603892+movingrock@users.noreply.github.com>
2024-03-27 16:09:16 +0900

    [level 1] Title: 폰켓몬, Time: 0.32 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <set>
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    int answer = 0;
+    
+    set<int> s;
+    for(int i = 0; i<nums.size(); i++){
+        s.insert(nums[i]);
+    }
+    
+    if(s.size() > nums.size() / 2) answer = nums.size()/2;
+    else answer = s.size();
+
+    return answer;
+}
