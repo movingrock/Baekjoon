3375b7dd44cb8ad240853c3958de9ad11e93719c
movingrock <75603892+movingrock@users.noreply.github.com>
2024-04-09 21:41:44 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+long long dp[91];
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	int n;
+	cin >> n;
+	dp[0] = 0;
+	dp[1] = 1;
+	for (int i = 2; i <= n; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+	cout << dp[n];
+
+	return 0;
+}
