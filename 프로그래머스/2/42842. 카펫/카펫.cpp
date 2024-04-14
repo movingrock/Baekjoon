2ebbd875205174a589d3df973c5fb8e21b5c1cba
movingrock <75603892+movingrock@users.noreply.github.com>
2024-04-14 19:06:17 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+    vector<int> answer;
+    
+    for(int x= 1; x<=max(brown, yellow); x++){ // 가로 길이 x라고 했을때
+        int y = (brown - 4)/2 - x;
+        cout << "x: " << x << " y: " << y << endl;
+        if(x * y == yellow){
+            answer.push_back(y + 2);
+            answer.push_back(x + 2);
+            break;
+        }
+    }
+    
+    return answer;
+}
