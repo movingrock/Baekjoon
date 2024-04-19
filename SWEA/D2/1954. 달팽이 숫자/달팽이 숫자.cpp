06051960c847d14fcb10d3721b02f9cd3e0a5f1c
movingrock <75603892+movingrock@users.noreply.github.com>
2024-04-19 15:38:35 +0900

    [D2] Title: 달팽이 숫자, Time: 11 ms, Memory: 13,544 KB -BaekjoonHub

---
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.cpp
@@ -0,0 +1,86 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// float b, c;
+// double d, e, f;
+// char g;
+// char var[256];
+// long long AB;
+// cin >> a;                            // int 변수 1개 입력받는 예제
+// cin >> b >> c;                       // float 변수 2개 입력받는 예제 
+// cin >> d >> e >> f;                  // double 변수 3개 입력받는 예제
+// cin >> g;                            // char 변수 1개 입력받는 예제
+// cin >> var;                          // 문자열 1개 입력받는 예제
+// cin >> AB;                           // long long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// float b = 1.0, c = 2.0;               
+// double d = 3.0, e = 0.0; f = 1.0;
+// char g = 'b';
+// char var[256] = "ABCDEFG";
+// long long AB = 12345678901234567L;
+// cout << a;                           // int 변수 1개 출력하는 예제
+// cout << b << " " << c;               // float 변수 2개 출력하는 예제
+// cout << d << " " << e << " " << f;   // double 변수 3개 출력하는 예제
+// cout << g;                           // char 변수 1개 출력하는 예제
+// cout << var;                         // 문자열 1개 출력하는 예제
+// cout << AB;                          // long long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+
+using namespace std;
+
+int n;
+
+bool InRange(int x, int y){
+ 	return x>=0 && x<n && y>=0 && y<n;   
+}
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+
+	cin>>T;
+
+    int dx[4] = {0, 1, 0, -1};
+    int dy[4] = {1, 0, -1, 0};
+    int grid[10][10] = {0, };
+    
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+        for(int i= 0; i<n; i++){
+        	for(int j = 0; j<n; j++){
+             	grid[i][j] = 0;   
+            }
+        }
+        cin >> n;
+
+        int dir = 0;
+        int cnt =1;
+        int x= 0;
+        int y = 0;
+        while(cnt < n*n+1){
+         	grid[x][y] = cnt++;
+			if(!InRange(x+dx[dir] , y+dy[dir]) || grid[x+dx[dir]][y+dy[dir]] > 0){
+                dir = (dir+1)%4;
+            }
+            
+            x += dx[dir];
+            y += dy[dir];
+            
+        }
+		cout << "#" << test_case << "\n";
+		for(int i= 0; i<n; i++){
+         	for(int j= 0; j<n; j++){
+                cout << grid[i][j] << " ";
+        	}
+        cout << endl;
+        }
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
