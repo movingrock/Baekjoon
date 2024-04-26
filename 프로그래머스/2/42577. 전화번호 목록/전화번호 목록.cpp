32cac866fd75045eb6f396a0f538f8e61e9c62d9
movingrock <75603892+movingrock@users.noreply.github.com>
2024-04-27 01:42:08 +0900

    [level 2] Title: 전화번호 목록, Time: 77.98 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,13 +1,12 @@
 #include <string>
 #include <vector>
-#include <set>
 #include <algorithm>
 
 using namespace std;
 
 bool solution(vector<string> phone_book) {
     bool answer = true;
-    set<string> s;
+
     sort(phone_book.begin(), phone_book.end());
     
     for(int i = 0; i<phone_book.size() - 1; i++){
