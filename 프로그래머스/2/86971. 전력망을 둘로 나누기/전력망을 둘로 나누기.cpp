3151c3614fee7867e9b916c0da41365d6ac8a8de
movingrock <75603892+movingrock@users.noreply.github.com>
2024-04-30 02:05:57 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.17 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -10,10 +10,10 @@ vector<int> v[101];
 
 int arr[100] = {0, };
 
-void bfs(int start, int color){
+void bfs(int start, int num){
     queue<int> q;
     q.push(start);
-    arr[start] = color;
+    arr[start] = num;
     
     while(!q.empty()){
         int x = q.front();
@@ -23,7 +23,7 @@ void bfs(int start, int color){
             int y = v[x][i];
             if(!arr[y]){
                 q.push(y);
-                arr[y] = color;
+                arr[y] = num;
             }
         }
     }
@@ -33,34 +33,37 @@ void bfs(int start, int color){
 int solution(int n, vector<vector<int>> wires) {
     int answer = INT_MAX;
     
-    for(int i= 0; i<wires.size(); i++){ // i번째 wire를 끊었다고 가정
+    // i번째 wire를 끊었다고 가정
+    for(int i= 0; i<wires.size(); i++){ 
         int num1 = 0;
         int num2 = 0;
+        
+        // vector, arr 초기화
         for(int j = 1; j<=n; j++){
             v[j].clear();
             arr[j] = 0;
         }
-        for(int j= 0; j<wires.size(); j++){ // j개 순환
+        
+        // i번째 wire를 제외하고 연결된 값을 넣어줌.
+        for(int j= 0; j<wires.size(); j++){
             if(i == j) continue;
             v[wires[j][0]].push_back(wires[j][1]);
             v[wires[j][1]].push_back(wires[j][0]);
         }
         
-        cout << wires[i][0] << " " << wires[i][1] << endl;
-                    
+        // bfs로 두 그래프를 1과 2로 나눔.
         bfs(wires[i][0], 1);
         bfs(wires[i][1], 2);
         
-    
-        
+        // 1과 2의 개수를 셈.
         for(int j = 1; j<=n; j++){
-            cout << arr[j] << " ";
             if(arr[j] == 1) num1++;
             else if(arr[j] == 2) num2++;
         }
-        cout << endl;
+        
+        // 두 집단의 개수 차의 최솟값을 반환
         answer = min(answer, abs(num1 - num2));
-        cout << "num1: " << num1 << " num2: " << num2 << endl;
+
     }
     
     
