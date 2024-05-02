47051ca36825f936fce09c843c4004856bfe64a5
movingrock <75603892+movingrock@users.noreply.github.com>
2024-05-02 16:39:23 +0900

    [level 4] Title: 사칙연산, Time: 2.51 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.cpp
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.cpp
@@ -12,8 +12,11 @@ int solution(vector<string> arr)
     int answer = -1;
     
     int num = arr.size() / 2 + 1;
+    
+    // i부터 j까지 구간 연산의 최대값과 최솟값
     for(int i =0 ; i<num; i++){
         for(int j = 0; j<num; j++){
+            // i, j가 같다면 
             if(i == j){
                 dp_max[i][j] = stoi(arr[i*2]);
                 dp_min[i][j] = stoi(arr[i*2]);
@@ -24,11 +27,12 @@ int solution(vector<string> arr)
         }
     }
 
-    for(int cnt = 1; cnt < num; cnt++){
+    for(int cnt = 1; cnt < num; cnt++){     // i와 j의 간격
         for(int i = 0; i<num-cnt; i++){
             int j = i + cnt;
             for(int k = i; k<j; k++){
                 if(arr[k*2 + 1] == "+"){
+                    // i부터 k까지의 값과 k+1부터 j까지의 값의 연산
                     dp_max[i][j] = max(dp_max[i][j], dp_max[i][k] + dp_max[k+1][j]);
                     dp_min[i][j] = min(dp_min[i][j], dp_min[i][k] + dp_min[k+1][j]);
                 }else if(arr[k*2+1] == "-"){
