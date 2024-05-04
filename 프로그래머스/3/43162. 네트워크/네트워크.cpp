6d408d5cc400c2fe60917c10edea9994cbe2d297
movingrock <75603892+movingrock@users.noreply.github.com>
2024-05-05 08:05:53 +0900

    [level 3] Title: 네트워크, Time: 0.04 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,9 +1,12 @@
 #include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 bool visited[201] = {0,};
+
+vector<int> v[201];
 
 void dfs(int x, vector<vector<int>>& computers){
     visited[x] = true;
@@ -14,13 +17,42 @@ void dfs(int x, vector<vector<int>>& computers){
     }
 }
 
+void bfs(int x, vector<vector<int>>& computers) {
+    queue<int> q;
+    q.push(x);
+    visited[x] = true;
+    
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
+        
+        for(int i=0; i<v[node].size(); i++){
+            int nNode = v[node][i];
+            if(!visited[nNode]){
+                q.push(nNode);
+                visited[nNode] = true;
+            }
+        }
+    }
+}
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
     
+    for(int i= 0; i<computers.size(); i++){
+        for(int j = 0; j<computers[0].size(); j++){
+            if(computers[i][j]){
+                v[i].push_back(j);
+                v[j].push_back(i);
+            }
+        }
+    }
+    
     for(int i= 0; i<n; i++){
         if(!visited[i]) {
-            dfs(i, computers); answer++;}
+            bfs(i, computers);
+            answer++;
+        }
     }
     
     return answer;
