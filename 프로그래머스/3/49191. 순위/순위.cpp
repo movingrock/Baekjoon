42f1d10fa4a8d825d54014a2641887a81aca4714
movingrock <75603892+movingrock@users.noreply.github.com>
2024-05-05 19:49:47 +0900

    [level 3] Title: 순위, Time: 1.31 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.cpp
+++ 프로그래머스/3/49191. 순위/순위.cpp
@@ -13,15 +13,16 @@ int solution(int n, vector<vector<int>> results) {
         v[results[i][0]][results[i][1]] = true;
     }
     
-    // a가 b를 이기고 b가 c를 이기면 a는 c를 이긴다
-    for(int i = 1; i<=n; i++){ // 중간노드
-        for(int j = 1; j<=n; j++){
+    // j가 i를 이기고  i가 k를 이겼다면, j는 k를 이긴것이다.
+    for(int i = 1; i<=n; i++){ // b
+        for(int j = 1; j<=n; j++){ 
             for(int k = 1; k<=n; k++){
                 if(v[j][i] && v[i][k]) v[j][k] = true;
             }
         }
     }
     
+    // 선수 세기
     for(int i = 1; i<=n; i++){
         int cnt = 0;
         for(int j = 1; j<=n; j++){
@@ -33,7 +34,3 @@ int solution(int n, vector<vector<int>> results) {
     
     return answer;
 }
-
-// 알 수 있는거
-// 경기를 n-1번 한 선수가 있다면 그 선수의 등수를 알 수 있음
-// 그 선수를 기준으로 이기거나 진 선수를 판별하고 그 중에서 또 알 수 있음
