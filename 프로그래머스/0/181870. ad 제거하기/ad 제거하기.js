1d0c3b79cec4b1b42445d74ef1985e37422ac464
movingrock <75603892+movingrock@users.noreply.github.com>
2024-07-11 12:03:54 +0900

    [level 0] Title: ad 제거하기, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.js
@@ -0,0 +1,7 @@
+function solution(strArr) {
+    var answer = [];
+    for(let i =0; i<strArr.length; i++){
+        if(!strArr[i].includes("ad")) answer.push(strArr[i]);
+    }
+    return answer;
+}
