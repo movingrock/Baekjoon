084323766c630c62182a320c17485151a568ca21
movingrock <75603892+movingrock@users.noreply.github.com>
2024-07-14 14:44:05 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[1001];
+int dp[1001];
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	int ans = 0;
+	for (int i = 0; i < n; i++) {
+		dp[i] = arr[i];
+		for (int j = 0; j < i; j++) {
+			if (arr[j] < arr[i]) {
+				dp[i] = max(dp[i], dp[j] + arr[i]);
+			}
+		}
+		ans = max(ans, dp[i]);
+	}
+
+	cout << ans;
+
+	return 0;
+}
